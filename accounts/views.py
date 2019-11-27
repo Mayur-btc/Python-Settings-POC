9c2e6b655e255006a3f4dd59d06754d17816ecce
Mayur-btc <mayur.prajapati@botreetechnologies.com>
2019-11-27 15:34:29 +0530

    change signup

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from .models import User
+from django.contrib.auth import authenticate,login
 # Create your views here.
-class UserCreateView(LoginRequiredMixin, CreateView):
+class UserCreateView(CreateView):
     model = User
-    fields = ['username', 'phone_number', 'gender',
-              'birthday', 'image']
+    fields = ['username','password', 'phone_number', 'gender', 'birthday', 'image']
+    def form_valid(self, form):
+        user = super().form_valid(form)
+        self.object.set_password(form.cleaned_data['password'])
+        self.object.save()
+        login(self.request, self.object)
+        return user
+        
--- settingsdemo/views.py
+++ settingsdemo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,HttpResponse
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class Home(View):
+class Home(LoginRequiredMixin,View):
     def get(self,request):
-        return render(request,'settingsdemo/index.html')
+        user = {'name':request.user}
+        print(request.user,request.user.id)
+        return render(request,'settingsdemo/index.html',context={'user':request.user})
 
